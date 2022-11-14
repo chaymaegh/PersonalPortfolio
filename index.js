44eb09d1decff55772adfbbffe870ddf39d4fa02
chaymaegh <94975733+chaymaegh@users.noreply.github.com>
2022-11-14 22:12:53 +0100

    Responsive Personal Portfolio Website

---
+++ index.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $("#menu").click(function(){
+        $(this).toggleClass("fa-times");
+        $("header").toggleClass("toggle");
+    });
+   
+    $(window).on("scroll load",function () {
+        $("menu").ramoveClass("fa-times");
+        $("header").removeClass("toggle");
+    });
+
+   // smooth scrolling
+   
+   $('a[href*="#"]').on('click',function(e){
+
+    e.preventDefault();
+
+    $('html, body').animate({
+
+      scrollTop : $($(this).attr('href')).offset().top,
+
+    },
+      500, 
+      'linear'
+    );
+
+  });
+
+
+
+
+});
